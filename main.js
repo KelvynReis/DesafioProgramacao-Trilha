909935647db068cf2198778dea6ab81409273319
Kelvyn Reis <kelvyn2reis@gmail.com>
2021-09-16 17:16:51 -0300

    update cultura.css

--- main.js
+++ main.js
@@ -10,5 +10,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); // troca de imagem a cada 4 segundos
 }
