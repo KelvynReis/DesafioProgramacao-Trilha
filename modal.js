0e217fd997041aa0d20ffc41d6ca6ffa622bbec2
Kelvyn Reis <kelvyn2reis@gmail.com>
2021-09-17 13:01:29 -0300

    create modal.html

---
+++ modal.js
@@ -0,0 +1,15 @@
+function iniciaModal(value){
+    const modal = document.getElementById(value);
+    if(modal){
+        modal.classList.add('exibir');
+        modal.addEventListener('click',(event)=>{
+            if(event.target.id == value || event.target.className == 'exit'){
+                modal.classList.remove('exibir');
+            }
+        })
+    }
+}
+
+const contato = document.querySelector('.contato');
+contato.addEventListener('click', ()=> iniciaModal('mymodal')
+)
